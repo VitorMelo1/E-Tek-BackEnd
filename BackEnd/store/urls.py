79cf794e60c10ea6302ec17a8ab23c08c5d86ee5
VitorMelo1 <vitormmelo04@gmail.com>
2025-05-15 09:25:03 -0300

    test: add test for get_product_by_id view

--- BackEnd/store/urls.py
+++ BackEnd/store/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import DeviceViewSet, AccessoryViewSet
+from .views import DeviceViewSet, AccessoryViewSet, populate_view
 
 router = DefaultRouter()
 router.register(r'devices', DeviceViewSet)
@@ -8,4 +8,5 @@ router.register(r'accessories', AccessoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('populate/', populate_view),  # ROTA TEMPORÁRIA pra popular o banco
 ]
--- BackEnd/store/views.py
+++ BackEnd/store/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Device, Accessory
 from .serializers import DeviceSerializer, AccessorySerializer
+from django.http import JsonResponse
+from .populate import run  # importa a função do populate
 
-# Create your views here.
+# ViewSets da API
 class DeviceViewSet(viewsets.ModelViewSet):
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
@@ -11,3 +13,8 @@ class DeviceViewSet(viewsets.ModelViewSet):
 class AccessoryViewSet(viewsets.ModelViewSet):
     queryset = Accessory.objects.all()
     serializer_class = AccessorySerializer
+
+# View temporária para popular o banco
+def populate_view(request):
+    run()
+    return JsonResponse({"status": "ok", "message": "Banco populado com sucesso!"})
